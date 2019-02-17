a0c53c516dc1d788c0687b1be976cddef31aa9ed
zhangsan <784950247@qq.com>
2019-02-17 20:08:48 +0800

    首个版本

---
+++ sparkmall-common/src/main/scala/com/atguigu/sparkmall0901/common/bean/CityInfo.scala
@@ -0,0 +1,9 @@
+package com.atguigu.sparkmall0901.common.bean
+
+case class CityInfo (city_id:Long,
+                     city_name:String,
+                     area:String
+                    ){
+
+}
+
